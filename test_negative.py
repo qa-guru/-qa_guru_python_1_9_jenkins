09884d02c2ee753f3abaef7ef2225501c2e350d8
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2022-07-19 20:32:33 +0300

    simple tests with allure-pytest

---
+++ test_negative.py
@@ -0,0 +1,22 @@
+def test_fail():
+    assert False
+
+
+def test_fail1():
+    assert False
+
+
+def test_fail2():
+    assert False
+
+
+def test_fail3():
+    assert False
+
+
+def test_fail4():
+    assert False
+
+
+def test_fail5():
+    assert False
