be09f23576f93893abf504125fd72959d2129b53
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2022-07-19 20:32:33 +0300

    simple tests with allure-pytest

---
+++ tests/simple/test_positive.py
@@ -0,0 +1,23 @@
+def test_success():
+    assert True
+
+def test_success1():
+    assert True
+
+def test_success2():
+    assert True
+
+def test_success3():
+    assert True
+
+def test_success4():
+    assert True
+
+def test_success4():
+    assert True
+
+def test_success5():
+    assert True
+
+def test_success6():
+    assert True
